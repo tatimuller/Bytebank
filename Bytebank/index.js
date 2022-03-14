5020ebae4c8a4ec60436b132d3d63d29d8da393d
Tatiane Muller <tatiane.muller@tributario.com.br>
2022-03-14 08:15:30 -0300

    Initial commit

---
+++ Bytebank/index.js
@@ -0,0 +1,21 @@
+ class Client{
+  name;
+  cpf;
+  agency;
+  balance;
+}
+const client1 = new Client();
+const client2 = new Client();
+
+client1.name = "João";
+client1.cpf = 15668458598;
+client1.agency = 5542;
+client1.balance = 0;
+console.log(client1);
+
+client2.name = "Claudia";
+client2.cpf = 756687858598;
+client2.agency = 8547;
+client2.balance= 10;
+console.log(client2);
+
